2d65f3f130c13f20c8f4f07006ddde603b81a15e
anurag9034 <kamanurag5@gmail.com>
2022-05-04 10:21:50 -0700

    line 36

--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { createStore, applyMiddleware } from 'redux';
-import { Provider } from 'react-redux';
-import thunk from 'redux-thunk';
-import { BrowserRouter } from 'react-router-dom';
-import allReducers from 'app/redux/redcuers';
-import App from './app/App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
+import { BrowserRouter } from 'react-router-dom'
+import allReducers from 'app/redux/redcuers'
+import App from './app/App'
 
-const store = createStore(allReducers, applyMiddleware(
-  thunk
-));
+const store = createStore(allReducers, applyMiddleware(thunk))
 
 ReactDOM.render(
   <React.StrictMode>
@@ -20,15 +18,17 @@ ReactDOM.render(
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
-);
+)
 
 if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-      navigator.serviceWorker.register('service-worker.js').then(registration => {
-        console.log('SW registered');
-      }).catch(registrationError => {
-        console.log('SW registration failed');
-      });
-    });
+  window.addEventListener('load', () => {
+    navigator.serviceWorker
+      .register('service-worker.js')
+      .then((registration) => {
+        console.log('SW registered')
+      })
+      .catch((registrationError) => {
+        console.log('SW registration failed')
+      })
+  })
 }
-
